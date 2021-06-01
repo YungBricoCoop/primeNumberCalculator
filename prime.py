eb2ffe02fc0c0676c0d8990712a179f52705cec2
Elwan Mayencourt <42273436+YungBricoCoop@users.noreply.github.com>
2021-06-01 20:53:32 +0200

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,5 @@
 inlinePrimeNumber = ', '.join([str(x[0]) for x in [[x for x in range(2,y-1) if y%x==0] for y in range(0,1000)] if len(x) == 1])
-print("Primer Numbers : "+inlinePrimeNumber)
+print("Prime Numbers : "+inlinePrimeNumber)
 x = 0
 while True:
 	x+=1
